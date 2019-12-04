7f8917a90542862b6b6ef90468969743ac981f0c
Lu Zhang <lz2732@columbia.edu>
2019-12-04 20:19:42 +0000

    views

--- tracker/app/views.py
+++ tracker/app/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Pet
+from .models import Sighting
 # Create your views here.
 def  all_squirrel_sightings(request):
     sightings=Sighting.objects.all()
     context={
             'sightings':sightings}
     return render(request, 'app/all.html',context) 
-def index(request):
+def sighting_details(request,Unique_Squirrel_ID):
+    sight=Sighting.objects.get(id=Unique_Squirrel_ID)
     return HttpResponse("Hello, world. You're at the polls index.")
