09dcb61bfbd9c1cea85a00d24f7aa52a39e41109
Lu Zhang <lz2732@columbia.edu>
2019-12-05 19:47:19 +0000

    Edit

--- tracker/app/management/commands/import_squirrel_data.py
+++ tracker/app/management/commands/import_squirrel_data.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
         parser.add_argument('path', type = str)
     
     def handle(self,*args,**kwargs):
-        import csv datetime
+        import csv, datetime
         def convert(string):
             if string == 'FALSE':
                 return False
